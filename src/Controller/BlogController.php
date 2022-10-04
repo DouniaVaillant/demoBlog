71c4d228bfc52859f46b9715aa6c5ff4ba561a84
DouniaVaillant <dounia.vaillant@gmail.com>
2022-10-04 16:19:33 +0200

    Version à garder de côté

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -3,7 +3,9 @@
 namespace App\Controller;
 
 use App\Entity\Article;
+use App\Entity\Comment;
 use App\Form\ArticleType;
+use App\Form\CommentType;
 use App\Repository\ArticleRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
@@ -13,105 +15,120 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class BlogController extends AbstractController
 {
-    
-
-    #[Route('/blog/show/{id}', name:'blog_show')]
-    public function show($id, ArticleRepository $repo)   //$id correspond à {id} dans l'URL
-    {
-        $articles = $repo->find($id);
-        //find() permet de récupérer un article en fonction de son id
-
-        return $this->render('blog/show.html.twig', [
-            'item' => $articles
-        ]);
-        
-    }
-
 
     #[Route('/', name: 'home')]
-
-    public function home ()
+    public function home()
     {
         return $this->render('blog/home.html.twig', [
             'slogan' => "La démo d'un Blog",
             'age' => 28
-            // pour envoyer des variables depuis le controller, la méthode render() prend en 2ème argument un tableau associatif
+            /// pour envoyer des variables depuis le controller, la méthode render() prend en 2ème argument un tableau associatif
         ]);
     }
-    
-    #[Route('/blog', name: 'app_blog')]
-    // Une route est définie par 2 arguments : son chemin (/blog) et son nom(app_blog)
-    // Allez sur une route permet de lancer la méthode qui se trouve directement en-dessous
 
-    // Les méthodes d'un controller renvoient TOUJOURS un objet de classe Reponse
+// -                                                                                                                                                    - //
+
+    #[Route('/blog', name: 'app_blog')]
+    /// Une route est définie par 2 arguments : son chemin (/blog) et son nom(app_blog)
+    /// Allez sur une route permet de lancer la méthode qui se trouve directement en-dessous
+    /// Les méthodes d'un controller renvoient TOUJOURS un objet de classe Reponse
     public function index(ArticleRepository $repo): Response
     {
-        // pour récipérer le repository, je le passe en arg de la méthode index()
-        // Cela s'appelle une injection de dépendance
+        /// pour récipérer le repository, je le passe en arg de la méthode index()
+        /// Cela s'appelle une injection de dépendance
 
-        $article =$repo->findAll();
-        // j'utilise findAll() por récupérer tous les articles
+        $article = $repo->findAll();
+        /// j'utilise findAll() por récupérer tous les articles
         return $this->render('blog/index.html.twig', [
-            'article' => $article, // j'envoie les articles au template
+            'article' => $article, /// j'envoie les articles au template
         ]);
-        
-        // render() permet d'afficher le contenu d'un template
+
+        /// render() permet d'afficher le contenu d'un template
     }
 
-    #[Route("/blog/new", name: "blog_create")]
-    #[Route("/blog/edit/{id}", name:"blog_edit")]
+// -                                                                                                                                                    - //
 
-    public function form (Request $globals, EntityManagerInterface $manager, Article $article = null)
+    #[Route('/blog/show/{id}', name: 'blog_show')]
+    public function show($id, ArticleRepository $repo, Request $globals, EntityManagerInterface $manager): Response
+    //$id correspondant au {id} dans l'URL
     {
-        //la classe Request contient les données véhiculées par les superglobales ($_POST, $_GET, $_SERVER...)
-        if($article==null)
-        {
-            $article = new Article; // je crée n objet de la classe Article vide prêt à être rempli
+        $article = $repo->find($id);
+        //find() permet de récupérer l'article en fonction de son id
 
-
-            //Si $article est null, nous somme dans la route blog_create : nous devons créer un nouvel article
-            //sinon, n$article n'est pas null nous somme donc dans la route bloc_edit : nous récupérons l'article correspondant à l'id
-        }
-        $form = $this->createForm(ArticleType::class, $article); // Je lie le formulaire à mon objet $article
-        //createdForm() permet de récupérer un formulaire
+        $comment = new Comment;
+        $form = $this->createForm(CommentType::class, $comment);
 
         $form->handleRequest($globals);
 
-        //dump($globals); // permet d'afficher les données de l'objet $globals (comme var_dump())
-       // dump($article);
+        if ($form->isSubmitted() && $form->isValid()) {
 
-        if($form->isSubmitted() && $form->isValid())
-        {
-            $article->setCreatedAt(new \DateTime); //ajout de la date seulement à l'insertion d'un article
-            $manager->persist($article); //prépare l'insertion de l'article en bdd
-            $manager->flush(); // exécute la requête d'insertion 
-            
+            $comment->setCreatedAt(new \DateTime);
+            $comment->setArticle($article);
+            $comment->setAuthor($this->getUser());
+
+            $manager->persist($comment);
+            $manager->flush();
+
             return $this->redirectToRoute('blog_show', [
-                'id' => $article->getId()            
+                'id' => $article->getId()
             ]);
-            //cette méthode permet de nos rediriger vers la page de notre article rnouvellement crée
         }
-
-        return $this->renderForm("blog/form.html.twig", [
-            'formArticle' => $form,
-            'editMode' => $article -> getId() !==null
-
-            // si nous somme sur la route /new : editMode = 0
-            //sinon, editMode = 1
+        return $this->renderForm('blog/show.html.twig', [
+            'item' => $article,
+            'formComment' => $form
         ]);
     }
 
-    #[Route("/blog/delete/{id}", name:"blog_delete")]
+// -                                                                                                                                                    - //
 
-    public function delete($id,EntityManagerInterface $manager, ArticleRepository $repo)
+    #[Route("/blog/new", name: "blog_create")]
+    #[Route("/blog/edit/{id}", name: "blog_edit")]
+    public function form(Request $globals, EntityManagerInterface $manager, Article $article = null)
+    {
+        ///la classe Request contient les données véhiculées par les superglobales ($_POST, $_GET, $_SERVER...)
+        if ($article == null) {
+            $article = new Article; /// je crée n objet de la classe Article vide prêt à être rempli
+            ///Si $article est null, nous somme dans la route blog_create : nous devons créer un nouvel article
+            ///sinon, n$article n'est pas null nous somme donc dans la route bloc_edit : nous récupérons l'article correspondant à l'id
+        }
+        $form = $this->createForm(ArticleType::class, $article); /// Je lie le formulaire à mon objet $article
+        ///createdForm() permet de récupérer un formulaire
+        $form->handleRequest($globals);
+        ///dump($globals); /// permet d'afficher les données de l'objet $globals (comme var_dump())
+
+        /// dump($article);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $article->setCreatedAt(new \DateTime); ///ajout de la date seulement à l'insertion d'un article
+            $manager->persist($article); ///prépare l'insertion de l'article en bdd
+            $manager->flush(); /// exécute la requête d'insertion 
+
+            return $this->redirectToRoute('blog_show', [
+                'id' => $article->getId()
+            ]);
+            ///cette méthode permet de nos rediriger vers la page de notre article rnouvellement crée
+        }
+
+
+        return $this->renderForm("blog/form.html.twig", [
+            'formArticle' => $form,
+            'editMode' => $article->getId() !== null
+
+            /// si nous somme sur la route /new : editMode = 0
+            ///sinon, editMode = 1
+        ]);
+    }
+
+// -                                                                                                                                                    - //
+
+    #[Route("/blog/delete/{id}", name: "blog_delete")]
+    public function delete($id, EntityManagerInterface $manager, ArticleRepository $repo)
     {
         $article = $repo->find($id);
 
         $manager->remove($article); // prépare la suppression
         $manager->flush(); //exécute la suppression
-        
+
         return $this->redirectToRoute('app_blog'); // Redirection vers la liste des articles
     }
-
 }
-
--- src/DataFixtures/VoitureFixtures.php
+++ src/DataFixtures/VoitureFixtures.php
@@ -2,23 +2,40 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Type;
 use App\Entity\Voiture;
-use Doctrine\Bundle\FixturesBundle\Fixture;
+use EntityManager\Type\persist;
 use Doctrine\Persistence\ObjectManager;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\ORM\EntityManager;
+
 
 class VoitureFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        for ($i = 1; $i <= 15; $i++) {
-            $voiture = new Voiture;
+        // $product = new Product();
+        // $manager->persist($product);
+        $faker = \Faker\Factory::create('fr_FR');
+        for($i = 1; $i <=3; $i++)
+            {
+                $type = new Type;
+                $type->setTitre($faker->sentence(3, false))
+                     ->setNbRoues(rand(1,4));
 
-            $voiture->setMarque("Marque de la voiture n°$i")
-                ->setModele("Modèle de la voiture n°$i")
-                ->setPrix($i * 3.7)
-                ->setDescription("Description de la voiture n°$i");
-            $manager->persist($voiture);
-        }
+                $manager->persist($type);
+            for($j = 1; $j <= 5; $j++)
+            {   $k= $i+$j;
+                $prix = rand(1000, 100000) ;
+                $voiture = new Voiture;
+                $voiture->setMarque("marque n°$k")
+                        ->setModele("modèle n°$k")
+                        ->setPrix($prix)
+                        ->setDescription("description de ma voiture ta vue tu connais n°$k")
+                        ->setType($type);
+                $manager->persist($voiture);
+            }
+        } 
         $manager->flush();
     }
 }
